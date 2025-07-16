806fccb0608f989494fedccc246bfca1fdb49b68
HanseKim <kimbird2002@naver.com>
2025-07-16 16:52:22 +0900

    config: prettier, eslint 설정

--- moigoFront/App.tsx
+++ moigoFront/App.tsx
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
-import "./global.css"
+import './global.css';
 
 export default function App() {
   return (
-    <View className="flex-1 justify-center items-center bg-white dark:bg-black">
+    <View className="items-center justify-center flex-1 bg-white dark:bg-black">
       <Text className="mb-4 text-2xl font-bold text-black dark:text-white">ddd</Text>
       <StatusBar style="auto" />
     </View>
