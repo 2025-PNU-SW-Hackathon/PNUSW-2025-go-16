18f094496feb680f7001641766139bdbf9cf741e
HanseKim <kimbird2002@naver.com>
2025-07-16 15:16:42 +0900

    config: babel presets 변경

--- moigoFront/babel.config.js
+++ moigoFront/babel.config.js
@@ -1,15 +1,3 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        root: ['./'],
-        alias: {
-          '@': './src', // '@/App' → 'moigoFront/App'
-        },
-      },
-    ],
-    'nativewind/babel',
-  ],
+  presets: ['@react-native/babel-preset'],
 };
