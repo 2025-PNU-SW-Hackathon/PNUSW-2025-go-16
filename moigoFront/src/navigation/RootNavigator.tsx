8bf3a9cb9573442d483c6522cb1183a77ba60612
HanseKim <kimbird2002@naver.com>
2025-07-17 12:19:27 +0900

    feat: 레이아웃 생성 및 이동

--- moigoFront/src/navigation/RootNavigator.tsx
+++ moigoFront/src/navigation/RootNavigator.tsx
@@ -4,29 +4,28 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import MainTabNavigator from './MainTabNavigator';
 import LoginScreen from '@/screens/LoginScreen';
 import SignupScreen from '@/screens/SignupScreen';
-
-export type RootStackParamList = {
-  Main: undefined;
-  Login: undefined;
-  Signup: undefined;
-};
+import type {RootStackParamList} from '@/types/RootStackParamList';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function RootNavigator() {
   // 예시: 실제로는 로그인 상태에 따라 분기
-  const isLoggedIn = false; // TODO: 실제 로그인 상태로 변경
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
 
-  return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      {isLoggedIn ? (
-        <Stack.Screen name="Main" component={MainTabNavigator} />
-      ) : (
-        <>
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="Signup" component={SignupScreen} />
-        </>
-      )}
-    </Stack.Navigator>
-  );
+return (
+  <Stack.Navigator screenOptions={{ headerShown: false }}>
+    {isLoggedIn ? (
+      <Stack.Screen name="Main">
+        {() => <MainTabNavigator />}
+      </Stack.Screen>
+    ) : (
+      <>
+        <Stack.Screen name="Login">
+          {() => <LoginScreen onLogin={() => setIsLoggedIn(true)} />}
+        </Stack.Screen>
+        <Stack.Screen name="Signup" component={SignupScreen} />
+      </>
+    )}
+  </Stack.Navigator>
+);
 }
