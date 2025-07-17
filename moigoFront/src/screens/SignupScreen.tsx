d02b31e943bafd8e067027bdf941d380f62a7c69
HanseKim <kimbird2002@naver.com>
2025-07-17 12:27:10 +0900

    fix: 화면이름 수정

--- moigoFront/src/screens/SignupScreen.tsx
+++ moigoFront/src/screens/SignupScreen.tsx
@@ -9,7 +9,7 @@ export default function SignupScreen() {
 
   return (
     <View className="flex-1 justify-center items-center bg-white">
-      <Text className="text-2xl font-bold text-mainOrange">로그인 화면</Text>
+      <Text className="text-2xl font-bold text-mainOrange">회원가입 화면</Text>
       <Button title="로그인" onPress={() => navigation.navigate('Login')} />
     </View>
   );
