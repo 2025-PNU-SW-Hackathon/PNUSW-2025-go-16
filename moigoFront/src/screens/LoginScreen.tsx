51c8436f9985d49d91e8388677c95e4b35fdfc62
HanseKim <kimbird2002@naver.com>
2025-07-17 12:25:18 +0900

    feat: 로그인 회원가입 이동 Navigator 추가

--- moigoFront/src/screens/LoginScreen.tsx
+++ moigoFront/src/screens/LoginScreen.tsx
@@ -5,10 +5,13 @@ import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import type { RootStackParamList } from '@/types/RootStackParamList';
 
 export default function LoginScreen({ onLogin }: { onLogin: () => void }) {
+  const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+
   return (
     <View className="flex-1 justify-center items-center bg-white">
       <Text className="text-2xl font-bold text-mainOrange">로그인 화면</Text>
       <Button title="로그인" onPress={onLogin} />
+      <Button title="회원가입" onPress={() => navigation.navigate('Signup')} />
     </View>
   );
 }
--- moigoFront/src/screens/SignupScreen.tsx
+++ moigoFront/src/screens/SignupScreen.tsx
@@ -1,11 +1,16 @@
-// src/screens/HomeScreen.tsx
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import type { RootStackParamList } from '@/types/RootStackParamList';
 
 export default function SignupScreen() {
+  const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+
   return (
-    <View>
-      <Text>회원가입 화면</Text>
+    <View className="flex-1 justify-center items-center bg-white">
+      <Text className="text-2xl font-bold text-mainOrange">로그인 화면</Text>
+      <Button title="로그인" onPress={() => navigation.navigate('Login')} />
     </View>
   );
 }
