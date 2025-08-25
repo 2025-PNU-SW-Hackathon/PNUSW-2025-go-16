d573a2b6fdf759310be899d765de04a404460099
yejun2 <yejun20020313@gmail.com>
2025-08-26 02:32:29 +0900

    내프로필 조회에 이미지 에러 수정

--- moigoBack/src/controllers/user_controller.js
+++ moigoBack/src/controllers/user_controller.js
@@ -133,10 +133,14 @@ exports.getMyProfile = async (req, res, next) => {
   try {
     const user_id = req.user.user_id;
     const data = await userService.getMyProfile(user_id);
-    const enrichedData = data.map(item => ({
-      ...item,
+    if (!data) {
+      return res.status(404).json({ success: false, message: "사용자를 찾을 수 없습니다." });
+    }
+
+    const enrichedData = {
+      ...data,
       user_thumbnail: `/api/v1/users/${user_id}/thumbnail`
-    }));
+    };
     res.status(200).json({ success: true, data : enrichedData });
   } catch (err) {
     next(err);
